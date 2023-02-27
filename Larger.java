f5e9cea2998a06343ef503a3ef6ef42d151586ed
kajalkhirodkar <kajalkhirodkar@gmail.com>
2023-02-27 14:28:49 +0530

    finding larger num

---
+++ Larger.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Larger {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers:");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } 
+        else {
+            System.out.println(b + " is greater than " + a);
+        }
+
+        sc.close();
+    
+    }
+
+}
